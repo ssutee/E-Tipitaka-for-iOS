422c90818beb30a9fabea29f06f998d17cdf21b8
Sutee Sudprasert <etipitaka@gmail.com>
2011-09-09 10:33:12 +0700

    added GHUnit

---
+++ Tests/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Tests
+//
+//  Created by Sutee Sudprasert on 9/9/54 BE.
+//  Copyright 2554 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    [pool release];
+    return retVal;
+}
